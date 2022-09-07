a5ed04d09acd4f125c837fe3a8eeda51e7f808e6
alhimik77 <alhimik878@gmail.com>
2022-09-07 22:23:48 +0300

    6. Create Node.JS Server  1. run npm init root folder  2. Update package.json: set type module  3. Add.js to imports  4. npm install express  5. Create server.js  6. add start command as node backend/server.js  7. require express in server.js  8. create route for / return backend is ready  9. move products.js from frontend to backend  10. create route for /api/products  11. return products  12. run npm star

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 function App() {
-  return (
-    <div>
-      <header>
-             <a href="/">amazon</a>
-      </header>
-        <main>
-                  list product
-        </main>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    <Link to="/">amazon</Link>
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/product/:slug" element={<ProductScreen/>}/>
+                        <Route path="/" element={<HomeScreen/>}/>
+                    </Routes>
+                </main>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
