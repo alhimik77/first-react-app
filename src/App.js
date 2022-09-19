438b795c2edf08130da7bf5441ef90f5be300e4f
alhimik77 <alhimik878@gmail.com>
2022-09-19 22:19:48 +0300

    23. Complete Sign-in Screen 1.Handle submit action 2.Save token in store and local storage 3.Show user name in header

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from "./screen/ProductScreen";
-import {Badge, Container, Nav, Navbar} from "react-bootstrap";
+import {Badge, Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 import {LinkContainer} from "react-router-bootstrap";
 import {useContext} from "react";
 import {Store} from "./Store";
 import CartScreen from "./screen/CartScreen";
 import SigninScreen from "./screen/SigninScreen";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-    const {state} = useContext(Store);
-    const {cart} = state;
+    const { state, dispatch: ctxDispatch} = useContext(Store);
+    const {cart, userInfo} = state;
+
+    const signoutHandler = () => {
+        ctxDispatch({type: 'USER_SIGNOUT'});
+        localStorage.removeItem('userInfo');
+    }
+
     return (
         <BrowserRouter>
             <div className="d-flex flex-column site-container">
+
+                <ToastContainer position="bottom-center" limit={1} />
+
                 <header>
                     <Navbar bg="dark" variant="dark">
                         <Container>
@@ -26,10 +37,33 @@ function App() {
                                     Cart
                                     {cart.cartItems.length > 0 && (
                                         <Badge pill bg="danger">
-                                            {cart.cartItems.reduce((a,c) => a + c.quantity, 0)}
+                                            {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
                                         </Badge>
                                     )}
                                 </Link>
+                                {userInfo ? (
+                                    <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                                        <LinkContainer to="/profile">
+                                            <NavDropdown.Item>Profile</NavDropdown.Item>
+                                        </LinkContainer>
+                                        <LinkContainer to="/orderhistory">
+                                            <NavDropdown.Item>Order History</NavDropdown.Item>
+                                        </LinkContainer>
+                                        <NavDropdown.Divider/>
+                                        <Link
+                                            className="dropdown-item"
+                                        to="#signout"
+                                            onClick={signoutHandler}
+                                        >
+                                            Sign Out
+                                        </Link>
+
+                                    </NavDropdown>
+                                ) : (
+                                    <Link className="nav-link" to="/signin">
+                                        Sign In
+                                    </Link>
+                                )}
                             </Nav>
 
                         </Container>
--- src/screen/SigninScreen.js
+++ src/screen/SigninScreen.js
@@ -1,12 +1,48 @@
 import {Button, Container, Form} from "react-bootstrap";
 import {Helmet} from "react-helmet-async";
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
+import axios from "axios";
+import {useContext, useEffect, useState} from "react";
+import {Store} from "../Store";
+import {toast} from "react-toastify";
+import {getError} from "../utils";
 
 
 export default function SigninScreen() {
-    const { search } = useLocation();
+
+    const navigate = useNavigate();
+
+    const {search} = useLocation();
     const redirectInUrl = new URLSearchParams(search).get('redirect');
     const redirect = redirectInUrl ? redirectInUrl : '/';
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const {state, dispatch: ctxDispatch} = useContext(Store);
+
+    const {userInfo} = state;
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+        try {
+            const {data} = await axios.post('/api/users/signin', {
+                email,
+                password,
+            });
+            ctxDispatch({type: 'USER_SIGNIN', payload: data});
+            localStorage.setItem('userInfo', JSON.stringify(data));
+            navigate(redirect || '/');
+        } catch (err) {
+            toast.error(getError(err));
+        }
+    };
+
+    useEffect(() => {
+        if (userInfo) {
+            navigate(redirect);
+        }
+    }, [navigate, redirect, userInfo]);
 
     return (
         <Container className="small-container">
@@ -14,15 +50,17 @@ export default function SigninScreen() {
                 <title>Sign In</title>
             </Helmet>
             <h1 className="my-3">Sign In</h1>
-            <Form>
+            <Form onSubmit={submitHandler}>
                 <Form.Group className="mb-3" controlId="email">
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" required/>
+                    <Form.Control type="email" placeholder="Enter email" required
+                                  onChange={(e) => setEmail(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Enter password" required/>
+                    <Form.Control type="password" placeholder="Enter password" required
+                                  onChange={(e) => setPassword(e.target.value)}/>
                 </Form.Group>
 
                 <div className="mb-3">
@@ -30,7 +68,7 @@ export default function SigninScreen() {
                 </div>
 
                 <div className="mb-3">
-                   New customer?{' '}
+                    New customer?{' '}
                     <Link to={`/signup?redirect=${redirect}`}>Create your account</Link>
                 </div>
 
