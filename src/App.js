5bfb887936b8052f98dcc130097b8b29ec8492a1
alhimik77 <alhimik878@gmail.com>
2022-10-19 16:32:37 +0300

    37. Manage Products Screen 1.Create product list ui 2.implement backend api 3.fetch data

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import SearchScreen from "./screen/SearchScreen";
 import ProtectedRoute from "./components/ProtectedRoute";
 import DashboardScreen from "./screen/DashboardScreen";
 import AdminRoute from "./components/AdminRoute";
+import ProductListScreen from "./screen/ProductListScreen";
 
 function App() {
 
@@ -120,13 +121,13 @@ function App() {
                                             <LinkContainer to="/admin/dashboard">
                                                 <NavDropdown.Item>Dashboard</NavDropdown.Item>
                                             </LinkContainer>
-                                            <LinkContainer to="/admin/productlist">
+                                            <LinkContainer to="/admin/products">
                                                 <NavDropdown.Item>Products</NavDropdown.Item>
                                             </LinkContainer>
-                                            <LinkContainer to="/admin/orderlist">
+                                            <LinkContainer to="/admin/order">
                                                 <NavDropdown.Item>Orders</NavDropdown.Item>
                                             </LinkContainer>
-                                            <LinkContainer to="/admin/userlist">
+                                            <LinkContainer to="/admin/users">
                                                 <NavDropdown.Item>Users</NavDropdown.Item>
                                             </LinkContainer>
                                         </NavDropdown>
@@ -201,6 +202,15 @@ function App() {
                                     <DashboardScreen/>
                                 </AdminRoute>
                             }/>
+
+                            <Route
+                                path="/admin/products"
+                                element={
+                                    <AdminRoute>
+                                        <ProductListScreen />
+                                    </AdminRoute>
+                                }/>
+
                             <Route path="/" element={<HomeScreen/>}/>
                         </Routes>
                     </Container>
