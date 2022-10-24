a37481c955baaed230a8f0874ba563bbe8de7892
alhimik77 <alhimik878@gmail.com>
2022-10-24 21:02:08 +0300

    47. Delete User 1.add delete button 2.handle click action 3.implement backend api for delete

--- src/screen/UserListScreen.js
+++ src/screen/UserListScreen.js
@@ -7,6 +7,7 @@ import {Store} from '../Store';
 import {getError} from '../utils';
 import Button from "react-bootstrap/Button";
 import {useNavigate} from "react-router-dom";
+import {toast} from "react-toastify";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -20,6 +21,29 @@ const reducer = (state, action) => {
             };
         case 'FETCH_FAIL':
             return {...state, loading: false, error: action.payload};
+        case 'DELETE_REQUEST':
+            return {
+                ...state,
+                loadingDelete: true,
+                successDelete: false,
+            };
+        case 'DELETE_SUCCESS':
+            return {
+                ...state,
+                loadingDelete: false,
+                successDelete: true,
+            };
+        case 'DELETE_FAIL':
+            return {
+                ...state,
+                loadingDelete: false,
+            };
+        case 'DELETE_RESET':
+            return {
+                ...state,
+                loadingDelete: false,
+                successDelete: false,
+            };
 
         default:
             return state;
@@ -28,7 +52,7 @@ const reducer = (state, action) => {
 export default function UserListScreen() {
 
     const navigate = useNavigate();
-    const [{loading, error, users}, dispatch] = useReducer(reducer, {
+    const [{loading, error, users, loadingDelete, successDelete}, dispatch] = useReducer(reducer, {
         loading: true,
         error: '',
     });
@@ -51,14 +75,36 @@ export default function UserListScreen() {
                 });
             }
         };
-        fetchData();
-    }, [userInfo]);
+        if (successDelete) {
+            dispatch({type: 'DELETE_RESET'});
+        } else {
+            fetchData();
+        }
+    }, [userInfo, successDelete]);
+
+    const deleteHandler = async (user) => {
+        if (window.confirm('Are you sure to delete?')) {
+            try {
+                dispatch({type: 'DELETE_REQUEST'});
+                await axios.delete(`/api/users/${user._id}`, {
+                    headers: {Authorization: `Bearer ${userInfo.token}`},
+                });
+                toast.success('User Deleted Successfully');
+                dispatch({type: 'DELETE_SUCCESS'});
+            } catch (err) {
+                toast.error(getError(err));
+                dispatch({type: 'FETCH_FAIL'});
+            }
+        }
+    }
+
     return (
         <div>
             <Helmet>
                 <title>Users</title>
             </Helmet>
             <h1>Users</h1>
+            {loadingDelete && <LoadingBox/>}
             {loading ? (
                 <LoadingBox></LoadingBox>
             ) : error ? (
@@ -87,6 +133,14 @@ export default function UserListScreen() {
                                     variant="light"
                                     onClick={() => navigate(`/admin/user/${user._id}`)}>
                                     Edit
+                                </Button>
+                                &nbsp;
+                                <Button
+                                    type={"button"}
+                                    variant="light"
+                                    onClick={() => deleteHandler(user)}
+                                >
+                                    Delete
                                 </Button>
                             </td>
                         </tr>
